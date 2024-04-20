4465da0e554218d558ffd2120d15bab3b90518b1
Himanshu Jain <himanshujain044@gmail.com>
2024-04-20 14:28:48 +0530

    fix: updated the cron job for removing a month brokerage

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -1,12 +1,15 @@
 const cron = require("node-cron");
 const Brokerage = require("../models/brokerage");
+const moment = require("moment");
 const { AMOUNT_PAID } = require("../constants/enum");
 
-// Schedule the cron job 24 hours daily
-const cronMoringDailyTimer = "0 0 */24 * * *"; 
-// const cronMoringDailyTimer = "*/5 * * * *";
+// Schedule the cron job at 3:30AM daily
+const cronStatusUpdateTimeExp = "30 16 * * *";
 
-cron.schedule(cronMoringDailyTimer, async () => {
+// Schedule the cron job at 4:30AM daily
+const cronRemoveBrokerageTimeExp = "30 17 * * *";
+
+cron.schedule(cronStatusUpdateTimeExp, async () => {
   try {
     await Brokerage.updateMany(
       { "brokerage.status": AMOUNT_PAID.PENDING },
@@ -14,7 +17,35 @@ cron.schedule(cronMoringDailyTimer, async () => {
       { arrayFilters: [{ "elem.status": AMOUNT_PAID.PENDING }] }
     );
     console.log(
-      "Brokerage Pedning to Padi status updated successfully !",
+      "Brokerage updated Pending to Paid status successfully !",
+      new Date().toString()
+    );
+  } catch (error) {
+    console.error("Error in cron job:", error);
+  }
+});
+
+cron.schedule(cronRemoveBrokerageTimeExp, async () => {
+  try {
+    const today = new Date();
+    const last30thDay = new Date(today);
+    last30thDay.setDate(today.getDate() - 30);
+    console.log(last30thDay.toDateString());
+    const data = await Brokerage.updateMany(
+      {},
+      {
+        $pull: {
+          brokerage: {
+            date: last30thDay,
+            status: AMOUNT_PAID.NOT_PAID,
+          },
+        },
+      }
+    );
+
+    console.log(data);
+    console.log(
+      "Brokerage updated, removed a month ago brokerage successfully !",
       new Date().toString()
     );
   } catch (error) {
